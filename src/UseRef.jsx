446a3a62aefe372374d0ae519ed8b59de736f1f2
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-10-25 15:14:50 +0200

    copy UseRef class from todays lesson

---
+++ src/UseRef.jsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useRef, useState} from 'react';
+
+function UseRef(props) {
+    const [inputValue, setInputValue] = useState("")
+    //const [previousInputValue, setPreviousValue] = useState("")
+    const previousInputValue = useRef("")
+    const inputRef = useRef("Hello")
+    const previousValue = useRef("")
+
+    const changeHandler = (e) => {
+        console.log(inputRef.current.value)
+    }
+    const focusHandler = () => {
+        inputRef.current.focus()
+    }
+
+    useEffect(() => {
+        previousInputValue.current = inputValue
+        //previousValue.current = inputValue
+    }, [inputValue])
+
+    return (
+        <div>
+            <input ref={inputRef} onChange={changeHandler}/>
+            <button onClick={focusHandler}>Focus</button>
+
+
+
+            <input type="text" value={inputValue} onChange={(e) => {
+                setInputValue(e.target.value)
+            }}/>
+            <p>Current Value: {inputValue}</p>
+            <p>Previous Value: {previousInputValue.current}</p>
+
+        </div>
+    );
+}
+
+export default UseRef;
