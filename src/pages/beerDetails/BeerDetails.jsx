bd27364431eda3ff44f7d9998764b8a43bc713ff
sebastian1234 <slarvad@icloud.com>
2022-10-27 17:46:19 +0200

    final touche

--- src/pages/beerDetails/BeerDetails.jsx
+++ src/pages/beerDetails/BeerDetails.jsx
@@ -10,6 +10,7 @@ const BeerDetails = (props) => {
     console.log(props, "props");
     console.log(location, "useLocation Hook")
     const beer = location.state.data
+    
   return (
     
     <div>
@@ -18,18 +19,20 @@ const BeerDetails = (props) => {
           <div className="back"></div>
           <div className='items'>
             
-            <Link className='return' to="/"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
-            </svg></Link>
+            <Link className='return' to="/">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+              </svg>
+            </Link>
+
+
 
             <img className='leftBeer' src={beer.image}></img>
             <div className='rightBeer'>
               <p className='beerTitle'>{beer.name}</p>
               <p className='beerSubTitle'>by {beer.brewery}</p>
-
-
-
             </div>
+            <div className="showScore"></div>
           </div>
         </div>
   
